04d41055860db3d8e1d3e28684d73a03f0a1a55e
Robert Laws <roblaws@me.com>
2023-01-31 14:52:50 +0300

    add basic pages and components

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+// Public Pages
+export { Home } from './Home';
+export { About } from './About';
+export { Contact } from './Contact';
+export { Publications } from './Publications';
+
+// 404
+export { NotFound } from './NotFound';
